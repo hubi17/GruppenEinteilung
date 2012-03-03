d5695237b95cfe3380955705863ac1e729970a93
unknown <hubi@hubi-PC.(none)>
2012-03-03 12:07:32 +0100

    initial commit

---
+++ 20120301_gruppenEinteilung/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace _20120301_gruppenEinteilung {
+    partial class Form1 {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pnlInput = new System.Windows.Forms.Panel();
+            this.lblSolo = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblAufgabe2 = new System.Windows.Forms.Label();
+            this.lblAufgabe1 = new System.Windows.Forms.Label();
+            this.lblSchueler = new System.Windows.Forms.Label();
+            this.nudGruppen = new System.Windows.Forms.NumericUpDown();
+            this.lblGruppen = new System.Windows.Forms.Label();
+            this.nudSchueler = new System.Windows.Forms.NumericUpDown();
+            this.lblAnzahl = new System.Windows.Forms.Label();
+            this.btnEingeben = new System.Windows.Forms.Button();
+            this.pnlAuswahl = new System.Windows.Forms.Panel();
+            this.btnEinteilen = new System.Windows.Forms.Button();
+            this.pnlInput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudGruppen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSchueler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlInput
+            // 
+            this.pnlInput.Controls.Add(this.lblSolo);
+            this.pnlInput.Controls.Add(this.btnClear);
+            this.pnlInput.Controls.Add(this.lblAufgabe2);
+            this.pnlInput.Controls.Add(this.lblAufgabe1);
+            this.pnlInput.Controls.Add(this.lblSchueler);
+            this.pnlInput.Controls.Add(this.nudGruppen);
+            this.pnlInput.Controls.Add(this.lblGruppen);
+            this.pnlInput.Controls.Add(this.nudSchueler);
+            this.pnlInput.Controls.Add(this.lblAnzahl);
+            this.pnlInput.Controls.Add(this.btnEingeben);
+            this.pnlInput.Location = new System.Drawing.Point(12, 12);
+            this.pnlInput.Name = "pnlInput";
+            this.pnlInput.Size = new System.Drawing.Size(428, 117);
+            this.pnlInput.TabIndex = 1;
+            // 
+            // lblSolo
+            // 
+            this.lblSolo.AutoSize = true;
+            this.lblSolo.Location = new System.Drawing.Point(303, 96);
+            this.lblSolo.Name = "lblSolo";
+            this.lblSolo.Size = new System.Drawing.Size(68, 13);
+            this.lblSolo.TabIndex = 22;
+            this.lblSolo.Text = "Einzelgruppe";
+            this.lblSolo.Visible = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(226, 43);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 21;
+            this.btnClear.Text = "Löschen";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblAufgabe2
+            // 
+            this.lblAufgabe2.AutoSize = true;
+            this.lblAufgabe2.Location = new System.Drawing.Point(198, 96);
+            this.lblAufgabe2.Name = "lblAufgabe2";
+            this.lblAufgabe2.Size = new System.Drawing.Size(56, 13);
+            this.lblAufgabe2.TabIndex = 20;
+            this.lblAufgabe2.Text = "Aufgabe 2";
+            this.lblAufgabe2.Visible = false;
+            // 
+            // lblAufgabe1
+            // 
+            this.lblAufgabe1.AutoSize = true;
+            this.lblAufgabe1.Location = new System.Drawing.Point(89, 96);
+            this.lblAufgabe1.Name = "lblAufgabe1";
+            this.lblAufgabe1.Size = new System.Drawing.Size(56, 13);
+            this.lblAufgabe1.TabIndex = 19;
+            this.lblAufgabe1.Text = "Aufgabe 1";
+            this.lblAufgabe1.Visible = false;
+            // 
+            // lblSchueler
+            // 
+            this.lblSchueler.AutoSize = true;
+            this.lblSchueler.Location = new System.Drawing.Point(8, 96);
+            this.lblSchueler.Name = "lblSchueler";
+            this.lblSchueler.Size = new System.Drawing.Size(43, 13);
+            this.lblSchueler.TabIndex = 18;
+            this.lblSchueler.Text = "Schüler";
+            this.lblSchueler.Visible = false;
+            // 
+            // nudGruppen
+            // 
+            this.nudGruppen.Enabled = false;
+            this.nudGruppen.Location = new System.Drawing.Point(97, 46);
+            this.nudGruppen.Name = "nudGruppen";
+            this.nudGruppen.Size = new System.Drawing.Size(104, 20);
+            this.nudGruppen.TabIndex = 17;
+            this.nudGruppen.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // lblGruppen
+            // 
+            this.lblGruppen.AutoSize = true;
+            this.lblGruppen.Enabled = false;
+            this.lblGruppen.Location = new System.Drawing.Point(8, 53);
+            this.lblGruppen.Name = "lblGruppen";
+            this.lblGruppen.Size = new System.Drawing.Size(75, 13);
+            this.lblGruppen.TabIndex = 16;
+            this.lblGruppen.Text = "Gruppengröße";
+            // 
+            // nudSchueler
+            // 
+            this.nudSchueler.Location = new System.Drawing.Point(97, 20);
+            this.nudSchueler.Name = "nudSchueler";
+            this.nudSchueler.Size = new System.Drawing.Size(104, 20);
+            this.nudSchueler.TabIndex = 15;
+            // 
+            // lblAnzahl
+            // 
+            this.lblAnzahl.AutoSize = true;
+            this.lblAnzahl.Location = new System.Drawing.Point(8, 25);
+            this.lblAnzahl.Name = "lblAnzahl";
+            this.lblAnzahl.Size = new System.Drawing.Size(78, 13);
+            this.lblAnzahl.TabIndex = 14;
+            this.lblAnzahl.Text = "Anzahl Schüler";
+            // 
+            // btnEingeben
+            // 
+            this.btnEingeben.Location = new System.Drawing.Point(226, 17);
+            this.btnEingeben.Name = "btnEingeben";
+            this.btnEingeben.Size = new System.Drawing.Size(75, 23);
+            this.btnEingeben.TabIndex = 13;
+            this.btnEingeben.Text = "Eingeben";
+            this.btnEingeben.UseVisualStyleBackColor = true;
+            this.btnEingeben.Click += new System.EventHandler(this.btnEingeben_Click);
+            // 
+            // pnlAuswahl
+            // 
+            this.pnlAuswahl.AutoScroll = true;
+            this.pnlAuswahl.Location = new System.Drawing.Point(13, 135);
+            this.pnlAuswahl.Name = "pnlAuswahl";
+            this.pnlAuswahl.Size = new System.Drawing.Size(546, 417);
+            this.pnlAuswahl.TabIndex = 2;
+            // 
+            // btnEinteilen
+            // 
+            this.btnEinteilen.Location = new System.Drawing.Point(13, 558);
+            this.btnEinteilen.Name = "btnEinteilen";
+            this.btnEinteilen.Size = new System.Drawing.Size(75, 23);
+            this.btnEinteilen.TabIndex = 3;
+            this.btnEinteilen.Text = "Einteilen";
+            this.btnEinteilen.UseVisualStyleBackColor = true;
+            this.btnEinteilen.Visible = false;
+            this.btnEinteilen.Click += new System.EventHandler(this.btnEinteilen_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 590);
+            this.Controls.Add(this.btnEinteilen);
+            this.Controls.Add(this.pnlAuswahl);
+            this.Controls.Add(this.pnlInput);
+            this.Name = "Form1";
+            this.Text = "Gruppenauswahl";
+            this.pnlInput.ResumeLayout(false);
+            this.pnlInput.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudGruppen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSchueler)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlInput;
+        private System.Windows.Forms.Label lblAufgabe2;
+        private System.Windows.Forms.Label lblAufgabe1;
+        private System.Windows.Forms.Label lblSchueler;
+        private System.Windows.Forms.NumericUpDown nudGruppen;
+        private System.Windows.Forms.Label lblGruppen;
+        private System.Windows.Forms.NumericUpDown nudSchueler;
+        private System.Windows.Forms.Label lblAnzahl;
+        private System.Windows.Forms.Button btnEingeben;
+        private System.Windows.Forms.Panel pnlAuswahl;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblSolo;
+        private System.Windows.Forms.Button btnEinteilen;
+
+
+    }
+}
+
---
+++ 20120301_gruppenEinteilung/Form1.cs
@@ -0,0 +1,242 @@
+﻿/*
+ * Program to generate random groups for given tasks.
+ * Written by Daniel Auhuber, FSI 1
+ * 01/03/2012
+ */
+
+/*
+ * TODO
+ * -wenn Anzahl SChüler erniedrigt wird, bestehende Einträge anpassen.
+ * -Schüler Namen anstatt Nr.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _20120301_gruppenEinteilung {
+
+    public partial class Form1 : Form {
+
+        const int DEFAULTGRUPPEN = 2;
+        int anzahlSchueler = 0;
+        int gruppenGroesse = 0;
+        SchuelerSelection[] mSchueler;
+        string[,] mAuswahl;
+        string[,] mAuswahlAufgabe1;
+        string[,] mAuswahlAufgabe2;
+
+        public Form1() {
+            
+            InitializeComponent();
+            nudGruppen.Value = DEFAULTGRUPPEN;
+        }
+
+        public class SchuelerSelection {
+
+            private GroupBox gbxSelection;
+            private Label lblSchueler;
+            private RadioButton rbtnAufgabe1;
+            private RadioButton rbtnAufgabe2;
+            private CheckBox cbxSolo;
+            //checkbox fuer einzelgruppe
+
+            public SchuelerSelection() {
+
+                gbxSelection = new GroupBox();
+                lblSchueler = new Label();
+                rbtnAufgabe1 = new RadioButton();
+                rbtnAufgabe2 = new RadioButton();
+                cbxSolo = new CheckBox();
+            }
+
+            public SchuelerSelection(int pSchueler, int pLeft, int pTop) {
+
+                gbxSelection = new GroupBox();
+                lblSchueler = new Label();
+                rbtnAufgabe1 = new RadioButton();
+                rbtnAufgabe2 = new RadioButton();
+                cbxSolo = new CheckBox();
+
+                gbxSelection.Left = pLeft;
+                gbxSelection.Top = pTop;
+                gbxSelection.Height = 40;
+                gbxSelection.Width = 335;
+                
+                lblSchueler.Left = 5;
+                lblSchueler.Top = 15;
+                
+                rbtnAufgabe1.Left = 100;
+                rbtnAufgabe1.Top = 10;
+                rbtnAufgabe1.Width = 20;
+                
+                rbtnAufgabe2.Left = 205;
+                rbtnAufgabe2.Top = 10;
+                rbtnAufgabe2.Width = 20;
+
+                cbxSolo.Left = 310;
+                cbxSolo.Top = 10;
+                cbxSolo.Width = 20;
+                
+                lblSchueler.Text = Convert.ToString(pSchueler);
+                rbtnAufgabe1.Text = "";
+                rbtnAufgabe2.Text = "";
+
+
+                gbxSelection.Controls.Add(rbtnAufgabe1);
+                gbxSelection.Controls.Add(rbtnAufgabe2);
+                gbxSelection.Controls.Add(lblSchueler);
+                gbxSelection.Controls.Add(cbxSolo);
+            }
+
+            public GroupBox getGroupBox() {
+
+                return gbxSelection;
+            }
+
+            public Label getSchueler() {
+
+                return lblSchueler;
+            }
+
+            public RadioButton getAufgabe1() {
+
+                return rbtnAufgabe1;
+            }
+
+            public RadioButton getAufgabe2() {
+
+                return rbtnAufgabe2;
+            }
+
+            public CheckBox getSolo() {
+
+                return cbxSolo;
+            }
+        }
+
+        private void btnEingeben_Click(object sender, EventArgs e) {
+
+            int topPos = 0;
+            
+
+            if (nudSchueler.Value > 0 && nudGruppen.Value > 0) {
+
+                lblSchueler.Visible = true;
+                lblAufgabe1.Visible = true;
+                lblAufgabe2.Visible = true;
+                lblSolo.Visible = true;
+                btnEinteilen.Visible = true;
+
+                anzahlSchueler = Convert.ToInt32(nudSchueler.Value);
+                gruppenGroesse = Convert.ToInt32(nudGruppen.Value);
+
+                mSchueler = new SchuelerSelection[anzahlSchueler];
+
+                for (int i = 0; i < anzahlSchueler; i++) {
+                    
+                    mSchueler[i] = new SchuelerSelection(i + 1, lblSchueler.Left, topPos);
+                    pnlAuswahl.Controls.Add(mSchueler[i].getGroupBox());
+                    topPos += 40;
+                }
+            } else {
+
+                lblSchueler.Visible = false;
+                lblAufgabe1.Visible = false;
+                lblAufgabe2.Visible = false;
+                lblSolo.Visible = false;
+                btnEinteilen.Visible = false;
+
+                pnlAuswahl.Controls.Clear();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e) {
+
+            lblSchueler.Visible = false;
+            lblAufgabe1.Visible = false;
+            lblAufgabe2.Visible = false;
+            lblSolo.Visible = false;
+            btnEinteilen.Visible = false;
+
+            pnlAuswahl.Controls.Clear();
+
+            gruppenGroesse = 0;
+            anzahlSchueler = 0;
+
+            nudGruppen.Value = DEFAULTGRUPPEN;
+            nudSchueler.Value = 0;
+        }
+
+        private void btnEinteilen_Click_1(object sender, EventArgs e) {
+
+            //TODO check that all radio buttons selected
+
+            int countAufgabe1 = 0;
+            int iterAufgabe1 = 0;
+            int countAufgabe2 = 0;
+            int iterAufgabe2 = 0;
+
+            mAuswahl = new string[anzahlSchueler, 4];
+
+            // Schleife zur Determinierung der Groesse der einzelnen Aufgabengruppen.
+            for (int i = 0; i < anzahlSchueler; i++) {
+                
+                if (mSchueler[i].getAufgabe1().Checked == true) {
+                    
+                    countAufgabe1++;
+                } else {
+                    
+                    if (mSchueler[i].getAufgabe2().Checked == true) {
+                        
+                        countAufgabe2++;
+                    }
+                }
+            }
+
+            MessageBox.Show("Aufgabe1 " + countAufgabe1);
+            MessageBox.Show("Aufgabe2 " + countAufgabe2);
+
+            mAuswahlAufgabe1 = new string[countAufgabe1, 2];
+            mAuswahlAufgabe2 = new string[countAufgabe2, 2];
+
+            // Schueler den jeweiligen Gruppen zuteilen.
+            for (int i = 0; i < anzahlSchueler; i++) {
+                
+                if (mSchueler[i].getAufgabe1().Checked == true) {
+                    
+                    mAuswahlAufgabe1[iterAufgabe1, 0] = Convert.ToString(mSchueler[i].getSchueler());
+                    mAuswahlAufgabe1[iterAufgabe1, 1] = Convert.ToString(mSchueler[i].getSolo().Checked);
+                    iterAufgabe1++;
+                } else {
+                    
+                    if (mSchueler[i].getAufgabe2().Checked == true) {
+                        
+                        mAuswahlAufgabe2[iterAufgabe2, 0] = Convert.ToString(mSchueler[i].getSchueler());
+                        mAuswahlAufgabe2[iterAufgabe2, 1] = Convert.ToString(mSchueler[i].getSolo().Checked);
+                        iterAufgabe2++;
+                    }
+                }
+            }
+
+            MessageBox.Show("Aufgabe1 " + iterAufgabe1);
+            MessageBox.Show("Aufgabe2 " + iterAufgabe2);
+
+            /*
+             * evtl keine volle liste aller benötigt
+            for (int i = 0; i < anzahlSchueler; i++) {
+                
+                mAuswahl[i, 0] = Convert.ToString(mSchueler[i].getSchueler());
+                mAuswahl[i, 1] = Convert.ToString(mSchueler[i].getAufgabe1().Checked);
+                mAuswahl[i, 2] = Convert.ToString(mSchueler[i].getAufgabe2().Checked);
+                mAuswahl[i, 3] = Convert.ToString(mSchueler[i].getSolo().Checked);
+            }
+             */
+        }
+    }
+}
